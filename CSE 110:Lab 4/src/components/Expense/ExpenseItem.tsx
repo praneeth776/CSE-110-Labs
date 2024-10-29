c73783cccf6fc6dc0da16008cd16943fe88c7370
praneeth776 <spraneeth536@gmail.com>
2024-10-29 13:44:06 -0700

    Updated ExpenseItem.tsx

--- CSE 110:Lab 4/src/components/Expense/ExpenseItem.tsx
+++ CSE 110:Lab 4/src/components/Expense/ExpenseItem.tsx
@@ -13,7 +13,7 @@ const ExpenseItem = (currentExpense: Expense) => {
   };
 
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
+    <li key={currentExpense.id} className="list-group-item d-flex justify-content-between align-items-center">
       <div>{currentExpense.name}</div>
       <div>${currentExpense.cost}</div>
       <div>
